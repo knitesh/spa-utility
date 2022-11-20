81bc8aef08dac50ddf4496658f885781cf1cfed3
Kumar Nitesh <contact@kumar-nitesh.com>
2022-11-20 16:15:32 -0500

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "nitex",
+    projectName: "utility",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      port: 9001,
+    },
+    externals: ["rxjs"],
+  });
+};
